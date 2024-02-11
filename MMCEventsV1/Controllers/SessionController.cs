4cd0203995e7d4b6fdfecca0dae52b2a5cb6cc76
Omar-Driouch <omardriouch29@gmail.com>
2024-02-11 14:37:45 +0100

    Updated

--- MMCEventsV1/Controllers/SessionController.cs
+++ MMCEventsV1/Controllers/SessionController.cs
@@ -5,12 +5,13 @@ using Microsoft.Extensions.Logging;
 using MMCEventsV1.DTO;
 using MMCEventsV1.Repository;
 using MMCEventsV1.Repository.Models;
+using System.ComponentModel.DataAnnotations;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MMCEventsV1.Controllers
 {
-    
+
     [Route("api/[controller]")]
     [ApiController]
     public class SessionController : ControllerBase
@@ -18,13 +19,13 @@ namespace MMCEventsV1.Controllers
         public readonly MMC_Event _Session;
         public SessionController(MMC_Event context)
         {
-            _Session= context;
+            _Session = context;
         }
         // GET: api/<SessionController> // Verified 
-      [HttpGet]
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<SessionResponseModel>>> Get()
         {
-            var sessions =  _Session.Sessions.ToList();
+            var sessions = _Session.Sessions.ToList();
             if (sessions == null || !sessions.Any())
             {
                 return NotFound();
@@ -38,12 +39,116 @@ namespace MMCEventsV1.Controllers
                 Description = session.Description,
                 Address = session.Address,
                 Picture = session.Picture,
-              
+
             }).ToList();
 
             return Ok(responseModels);
         }
 
+
+        //psot Sessions Verified 
+        [HttpPost("{EventID}")]
+        public async Task<ActionResult<string>> AddNewSession([FromBody] SessionInputModel sessionInputModel, int EventID)
+        {
+            try
+            {
+                if (sessionInputModel == null)
+                {
+                    return BadRequest("Data must not be null");
+                }
+                var findEvent = await _Session.Events.FindAsync(EventID);
+                if (findEvent==null)
+                {
+                    return NotFound("Event Does not exist");
+                }
+                Session NewSession = new Session
+                {
+                    Address = sessionInputModel.Address,
+                    Title = sessionInputModel.Title,
+                    DateSession = sessionInputModel.DateSession,
+                    Description = sessionInputModel.Description,
+                    Picture = sessionInputModel.Picture,
+                    EventId = EventID
+                };
+
+                await _Session.Sessions.AddAsync(NewSession);
+                await _Session.SaveChangesAsync();
+
+                return Ok("The session has been added successfully");
+            }
+            catch (Exception err)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while adding a new session." + err.Message);
+            }
+        }
+
+
+        //Get  Sessions by ID   // VERIFIED 
+        [HttpGet("{SessionID}")] 
+        public async Task<ActionResult<SessionResponseModel>> GetSessionByID(int SessionID)
+        {
+            try
+            {
+                var findSessionById= await _Session.Sessions.FindAsync(SessionID);
+
+                if (findSessionById == null)
+                {
+                    return NotFound("Session Not Found ");
+                }
+
+                SessionResponseModel FoundSession = new SessionResponseModel
+                {
+                    SessionID= findSessionById.SessionId,
+                    Address = findSessionById.Address,
+                    Title = findSessionById.Title,
+                    DateSession = findSessionById.DateSession,
+                    Description = findSessionById.Description,
+                    Picture = findSessionById.Picture,
+
+                };
+
+                 
+
+                return Ok(FoundSession);
+            }
+            catch (Exception err)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while adding a new session." + err.Message);
+            }
+        }
+
+
+        // HttpPut: api/<SessionController> // Verified
+        [HttpPut("{SessionID}")]
+        public async Task<ActionResult<string>> UpdateSession([FromBody] SessionInputModel sessionInputModel , int SessionID)
+        {
+            var SessionToUpdate = await _Session.Sessions.FindAsync(SessionID);
+            try
+            {
+                if (SessionToUpdate == null)
+                {
+                    return NotFound();
+                }
+
+                SessionToUpdate.Picture = sessionInputModel.Picture;
+                SessionToUpdate.Title = sessionInputModel.Title;
+                SessionToUpdate.Address = sessionInputModel.Address;
+                SessionToUpdate.DateSession = sessionInputModel.DateSession;
+                SessionToUpdate.Description = sessionInputModel.Description;
+
+                await _Session.SaveChangesAsync();
+                return Ok("the session has been updated successfully !");
+            }
+            catch (Exception err)
+            {
+
+                return BadRequest(err.Message);
+            }
+        }
+
+        // HttpDelete: api/<SessionController> // Verified 
         [HttpDelete("{SessionID}")]
         public async Task<ActionResult<string>> DeleteSession(int SessionID)
         {
---
+++ MMCEventsV1/DTO/SessionInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MMCEventsV1.DTO
+{
+    public class SessionInputModel
+    {
+        //public int SessionID { get; set; }
+        public string? Title { get; set; }
+        public DateTime? DateSession { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
+        public string? Picture { get; set; }
+    }
+}
